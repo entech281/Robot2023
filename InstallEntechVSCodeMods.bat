0d77d24bea0e1146a6259f4bcd62ea8258f12538
Michael Andrews <mandrews@mandrews.org>
2023-01-16 16:58:55 -0500

    Fix batch file errors and execute permissions

--- InstallEntechVSCodeMods.bat
+++ InstallEntechVSCodeMods.bat
@@ -1,13 +1,14 @@
 SET INSTALL_DIR="C:\Users\Public\wpilib\2023\vscode\data\extensions\wpilibsuite.vscode-wpilib-2023.1.1\resources\java\src\commands"
 
+@ECHO OFF
 IF NOT EXIST %INSTALL_DIR% (
-    ECHO Install directory not found -- Either install WPILib for this user or fix the script for updated version of WPILib
-    ECHO Install to %INSTALL_DIR%
-    GOTO :Exit
+    @ECHO Install directory not found -- Either install WPILib or fix the script for updated version of WPILib
+    @ECHO Install to %INSTALL_DIR%
+    GOTO :PauseExit
 )
 
-
-XCOPY vscode-resources/* %INSTALL_DIR% /S /R /Y
+@ECHO ON
+XCOPY vscode-resources\* %INSTALL_DIR% /S /R /Y
 
 :PauseExit
 PAUSE
