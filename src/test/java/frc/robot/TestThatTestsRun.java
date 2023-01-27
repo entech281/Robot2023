8f9314a0b42af6a49318969af8e681e92b03492d
Dave Cowden <dave.cowden@gmail.com>
2023-01-27 12:23:41 -0500

    added a single do-nothing test as an example

---
+++ src/test/java/frc/robot/TestThatTestsRun.java
@@ -0,0 +1,18 @@
+
+package frc.robot;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * 
+ *
+ * @author dcowden
+ */
+public class TestThatTestsRun {
+    
+    @Test
+    public void testThatThisIsExecuted(){
+    }
+    
+}
