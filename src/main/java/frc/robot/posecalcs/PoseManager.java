acd1f9503398cc12904fa95c96a299277cd273f1
EllieMah <mynameiselliemahaffey@gmail.com>
2023-01-28 14:01:56 -0500

    Create PoseManager.java
    created outline for PoseManager

---
+++ src/main/java/frc/robot/posecalcs/PoseManager.java
@@ -0,0 +1,12 @@
+package frc.robot.posecalcs;
+
+public class PoseManager {
+    private PoseCalcs poseCalcs;
+
+    public PoseManager() {
+    }
+
+    public PoseCalcs getPoseCalcs() {
+        return poseCalcs;
+    }
+}
