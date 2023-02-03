c1684db698ed10fb902943d36f2de29ce3e72a53
EllieMah <mynameiselliemahaffey@gmail.com>
2023-02-02 20:03:18 -0500

    Update RobotPose.java
    created Pose2d in RobotPose, armpose in progress

--- src/main/java/frc/robot/pose/RobotPose.java
+++ src/main/java/frc/robot/pose/RobotPose.java
@@ -1,10 +1,35 @@
 
 package frc.robot.pose;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 /**
  *
  * @author dcowden
  */
 public class RobotPose {
+
+    public RobotPose(Pose2d newPose){
+        pose = newPose;
+    }
+
+    private Pose2d pose;
+  
+    public Pose2d getPose(){
+        return pose;
+    }
+   
+    //arm pose code in progress
+    
+    //public RobotPose(/*Arm Pose*/ newPose){
+        //armPose = newpose;
+    //}
+
+    //private /*Arm Pose*/ armPose;
+
+    //public /*Arm Pose*/ getarmPose(){
+        //return armPose;
+    //}
     
 }
