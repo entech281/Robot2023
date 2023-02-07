97372bb6e25fa4e05d73d0c278d7a7ea557c1f07
EllieMah <mynameiselliemahaffey@gmail.com>
2023-02-06 19:55:25 -0500

    changed code in robot pose and navxpose
    However some code has not be added in other files to make the code added work properly - fails builds

--- src/main/java/frc/robot/pose/NavxPose.java
+++ src/main/java/frc/robot/pose/NavxPose.java
@@ -1,28 +1,37 @@
 package frc.robot.pose;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import frc.robot.util.SendableUtil;
 
 /**
  *
  * @author dcowden
  */
-public class NavxPose {
+public class NavxPose implements Sendable {
     
     public NavxPose(double angle, Pose2d newpose){
-        this.angle = angle;
-        this.pose = newpose;
+        this.yawAngleDegrees = angle;
+        this.basePose = newpose;
     }
 
-    private double angle = 0.0;
-    private Pose2d pose;
-    
-    
-    public double getAngle(){
-        return angle;
+    private double yawAngleDegrees = 0.0;
+    private Pose2d basePose;
+
+
+    public double getYawAngleDegrees(){
+        return yawAngleDegrees;
     }
 
-    public Pose2d getPose(){
-        return pose;
+    public Pose2d getBasePose(){
+        return basePose;
+    }
+
+    @Override
+    public void initSendable(SendableBuilder sb) {
+        sb.addDoubleProperty("Yaw Angle",this::getYawAngleDegrees, null);
+        SendableUtil.initPose2dSendable("Base", this::getBasePose, sb);
     }
     
 }
