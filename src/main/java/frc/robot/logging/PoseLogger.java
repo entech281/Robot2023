7408fc88305a922e054cbb0ef5e800c35b47ab47
Dave Cowden <dave.cowden@gmail.com>
2023-02-17 15:32:35 -0500

    add poses to shuffleboard

--- src/main/java/frc/robot/logging/PoseLogger.java
+++ src/main/java/frc/robot/logging/PoseLogger.java
@@ -1,11 +1,14 @@
 package frc.robot.logging;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class PoseLogger  {
 
 	public void logPose2d(String name, Pose2d p) {
-		SmartDashboard.putData( new Pose2dSendable(name, p));
+		Pose2dSendable ps = new Pose2dSendable(name, p);
+		SmartDashboard.putData( name,ps );
+		Shuffleboard.getTab("Poses").add(name, ps);
 	}
 }
