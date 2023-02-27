e73cda2de68bb86d5d96a1ead2142963d1a022e0
Andrew Heitkamp <phoenixnet.phoenix@gmail.com>
2023-02-27 18:10:17 -0500

    updates and motor types and can ids

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -74,10 +74,10 @@ public final class RobotConstants {
         }
     }
     public interface CAN {
-        public static final int FRONT_LEFT_MOTOR = 5;
-        public static final int REAR_RIGHT_MOTOR = 6;
-        public static final int FRONT_RIGHT_MOTOR = 7;
-        public static final int REAR_LEFT_MOTOR = 8;
+        public static final int FRONT_LEFT_MOTOR = 1;
+        public static final int REAR_RIGHT_MOTOR = 3;
+        public static final int FRONT_RIGHT_MOTOR = 2;
+        public static final int REAR_LEFT_MOTOR = 4;
         public static final int ELBOW_MOTOR_ID = 0;
         public static final int TELESCOPE_MOTOR_ID = 0;
     }
