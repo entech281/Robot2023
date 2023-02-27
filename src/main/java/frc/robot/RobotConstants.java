81f11594d4ba3da7f6ec81bbc555208ee14edccd
Andrew Heitkamp <phoenixnet.phoenix@gmail.com>
2023-02-27 18:10:17 -0500

    update drives for comp bots to sparkmax

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -133,12 +133,13 @@ public final class RobotConstants {
         }
     }
     public interface CAN {
-        public static final int FRONT_LEFT_MOTOR = 5;
-        public static final int REAR_RIGHT_MOTOR = 6;
-        public static final int FRONT_RIGHT_MOTOR = 7;
-        public static final int REAR_LEFT_MOTOR = 8;
-        public static final int ELBOW_MOTOR_ID = 3;
-        public static final int TELESCOPE_MOTOR_ID = 2;
+
+        public static final int FRONT_LEFT_MOTOR = 1;
+        public static final int REAR_RIGHT_MOTOR = 3;
+        public static final int FRONT_RIGHT_MOTOR = 2;
+        public static final int REAR_LEFT_MOTOR = 4;
+        public static final int ELBOW_MOTOR_ID = 0;
+        public static final int TELESCOPE_MOTOR_ID = 0;
     }
     public interface DIGITAL_IO {
 
