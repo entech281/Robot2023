d029b4d704e13e2cbce49b498f02ce9999ac1edb
EllieMah <mynameiselliemahaffey@gmail.com>
2023-02-28 20:27:26 -0500

    added elbow angle to tests - failing

--- src/main/java/frc/robot/pose/ArmPoseCalculator.java
+++ src/main/java/frc/robot/pose/ArmPoseCalculator.java
@@ -1,23 +1,30 @@
 package frc.robot.pose;
 
 import frc.robot.RobotConstants;
+import frc.robot.subsystems.ElbowStatus;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import java.lang.Math;
 
 public class ArmPoseCalculator {
     
-    public ArmPoseResult getDeploymentStatus(Pose2d estimatedPose, ScoringLocation target) {
+    public ArmPoseResult getDeploymentStatus(Pose2d estimatedPose, ScoringLocation target, ElbowStatus elbowAngle) {
 		Translation2d start = estimatedPose.getTranslation();
 		Translation2d end = target.computeAbsolutePose().getTranslation();
         ArmPoseResult armPose = new ArmPoseResult();
         armPose.setTargetDistance(end.getDistance(start));
-        armPose.setCanDeploy(canDeploy(armPose.targetDistance));
+        armPose.setCanDeploy(canDeploy(armPose.targetDistance, elbowAngle.getVerticalAngle()));
 		return armPose;
 	}
 
-    private boolean canDeploy(double targetDistance){
-        boolean tooFar = targetDistance > RobotConstants.ARM.MAX_EXTENSION_METERS;
-        boolean tooClose = targetDistance < RobotConstants.ARM.MIN_EXTENSION_METERS;
+    private boolean canDeploy(double targetDistance, double elbowAngle){
+       //calculate max horizontal arm reach
+        double maxHorizontalArmReach = RobotConstants.ARM.MAX_EXTENSION_METERS * Math.sin(elbowAngle);
+       //calculate min horizontal arm reach
+       double minHorizontalArmReach = RobotConstants.ARM.MIN_EXTENSION_METERS * Math.sin(elbowAngle);
+        boolean tooFar = targetDistance > maxHorizontalArmReach;
+        boolean tooClose = targetDistance < minHorizontalArmReach;
+        
         boolean canDeploy = !tooFar && !tooClose;
         return canDeploy;
     }
--- src/test/java/frc/robot/pose/TestArmPoseCalculator.java
+++ src/test/java/frc/robot/pose/TestArmPoseCalculator.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.subsystems.ElbowStatus;
+import frc.robot.RobotConstants;
+import frc.robot.RobotConstants.ELBOW;
 
 public class TestArmPoseCalculator {
 
@@ -13,9 +16,11 @@ public class TestArmPoseCalculator {
     public void testFacingScoringLocation(){
         ScoringLocation sloc = new ScoringLocation(AprilTagLocation.BLUE_LEFT, TargetNode.A2);
         Pose2d robotPose = new Pose2d(1.8, 1.07, new Rotation2d(180));
+        ElbowStatus elbowAngle = new ElbowStatus((int)(ELBOW.SETTINGS.COUNTS_PER_DEGREE * (90 - ELBOW.MIN_ANGLE_DEGREES)));
         ArmPoseCalculator armPose = new ArmPoseCalculator();
-        ArmPoseResult armPoseAnswer = armPose.getDeploymentStatus(robotPose, sloc);
+        ArmPoseResult armPoseAnswer = armPose.getDeploymentStatus(robotPose, sloc, elbowAngle);
 
+        System.out.println(armPoseAnswer);
         assertEquals(1.42, armPoseAnswer.targetDistance, TOLERANCE_DISTANCE);
         assertEquals(true, armPoseAnswer.canDeploy);
     }
@@ -24,8 +29,9 @@ public class TestArmPoseCalculator {
     public void testTooClose(){
         ScoringLocation sloc = new ScoringLocation(AprilTagLocation.BLUE_LEFT, TargetNode.A2);
         Pose2d robotPose = new Pose2d(0.5, 1.07, new Rotation2d(180));
+        ElbowStatus elbowAngle = new ElbowStatus((int)(ELBOW.SETTINGS.COUNTS_PER_DEGREE * (90 - ELBOW.MIN_ANGLE_DEGREES)));
         ArmPoseCalculator armPose = new ArmPoseCalculator();
-        ArmPoseResult armPoseAnswer = armPose.getDeploymentStatus(robotPose, sloc);
+        ArmPoseResult armPoseAnswer = armPose.getDeploymentStatus(robotPose, sloc, elbowAngle);
 
         assertEquals(0.12, armPoseAnswer.targetDistance, TOLERANCE_DISTANCE);
         assertEquals(false, armPoseAnswer.canDeploy);
@@ -35,8 +41,9 @@ public class TestArmPoseCalculator {
     public void testTooFar(){
         ScoringLocation sloc = new ScoringLocation(AprilTagLocation.BLUE_LEFT, TargetNode.A2);
         Pose2d robotPose = new Pose2d(2.0, 1.07, new Rotation2d(180));
+        ElbowStatus elbowAngle = new ElbowStatus((int)(ELBOW.SETTINGS.COUNTS_PER_DEGREE * (90 - ELBOW.MIN_ANGLE_DEGREES)));
         ArmPoseCalculator armPose = new ArmPoseCalculator();
-        ArmPoseResult armPoseAnswer = armPose.getDeploymentStatus(robotPose, sloc);
+        ArmPoseResult armPoseAnswer = armPose.getDeploymentStatus(robotPose, sloc, elbowAngle);
 
         assertEquals(1.62, armPoseAnswer.targetDistance, TOLERANCE_DISTANCE);
         assertEquals(false, armPoseAnswer.canDeploy);
