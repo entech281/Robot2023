f0f02f7c0c1d2870c2c9e360c130c4ca58c62f02
John-Molfetner <jtmolfenter07@gmail.com>
2023-03-14 19:04:22 -0400

    Command Factory Cleanup

--- src/main/java/frc/robot/CommandFactory.java
+++ src/main/java/frc/robot/CommandFactory.java
@@ -93,10 +93,9 @@ public class CommandFactory {
 			groundScoringElbowCommand(),
 			loadingElbowCommand(),
 			carryElbowCommand(),
-			new PositionTelescopeCommand(armSubsystem,1.4, false),
-			new PositionTelescopeCommand(armSubsystem,0.2, false),
-			new PositionTelescopeCommand(armSubsystem,0.08, false),
-
+			new PositionTelescopeCommand(armSubsystem,RobotConstants.ARM.POSITION_PRESETS.MAX_METERS, false),
+			new PositionTelescopeCommand(armSubsystem,RobotConstants.ARM.POSITION_PRESETS.SCORE_HIGH_METERS, false),
+			new PositionTelescopeCommand(armSubsystem,RobotConstants.ARM.POSITION_PRESETS.SCORE_MIDDLE_METERS, false),
 			new PositionElbowCommand(elbowSubsystem,RobotConstants.ELBOW.POSITION_PRESETS.MIN_POSITION_DEGREES, false),
 			new GripperCommand(gripperSubsystem,GripperState.kClose,"CloseGripper"),
 			new GripperCommand(gripperSubsystem,GripperState.kOpen,"OpenGripper"),
@@ -140,7 +139,7 @@ public class CommandFactory {
             , new DriveSetBrake(driveSubsystem)
             , new PositionTelescopeCommand(armSubsystem, RobotConstants.ARM.POSITION_PRESETS.MIN_METERS, true)            
             , new PositionElbowCommand(elbowSubsystem, RobotConstants.ELBOW.POSITION_PRESETS.SCORE_HIGH_DEGREES, true)
-            , new PositionTelescopeCommand(armSubsystem, 1.385, true)
+            , new PositionTelescopeCommand(armSubsystem, RobotConstants.ARM.POSITION_PRESETS.SCORE_HIGH_METERS, true)
             , new WaitCommand(1.0)
             , new GripperCommand(gripperSubsystem, GripperState.kOpen)
             , new WaitCommand(1.0)
@@ -154,8 +153,8 @@ public class CommandFactory {
     }
     public Command autonomusMiddleCommand() {
         return new SequentialCommandGroup(
-        new PositionElbowCommand(elbowSubsystem, 80, true)
-        , new PositionTelescopeCommand(armSubsystem, 1.1, true)
+        new PositionElbowCommand(elbowSubsystem, RobotConstants.ARM.POSITION_PRESETS.SCORE_MIDDLE_METERS, true)
+        , new PositionTelescopeCommand(armSubsystem, RobotConstants.ARM.POSITION_PRESETS.SCORE_MIDDLE_METERS, true)
         );
     }
 
@@ -272,7 +271,7 @@ public class CommandFactory {
             new ConditionalCommand(new InstantCommand(), groundRetractedPosition(), elbowSubsystem::isSafeToExtendArm),
             new ParallelCommandGroup(
                 new PositionElbowCommand(elbowSubsystem, RobotConstants.ELBOW.POSITION_PRESETS.SCORE_HIGH_DEGREES, true),
-                new PositionTelescopeCommand(armSubsystem, RobotConstants.ARM.POSITION_PRESETS.MAX_ARM_LENGTH_M, true)
+                new PositionTelescopeCommand(armSubsystem, RobotConstants.ARM.POSITION_PRESETS.SCORE_HIGH_METERS, true)
             )
         );
     }
@@ -282,7 +281,7 @@ public class CommandFactory {
             new ConditionalCommand(new InstantCommand(), groundRetractedPosition(), elbowSubsystem::isSafeToExtendArm),
             new ParallelCommandGroup(
                 new PositionElbowCommand(elbowSubsystem, RobotConstants.ELBOW.POSITION_PRESETS.SCORE_MIDDLE_DEGREES, true),
-                new PositionTelescopeCommand(armSubsystem, 1.1, true)
+                new PositionTelescopeCommand(armSubsystem, RobotConstants.ARM.POSITION_PRESETS.SCORE_MIDDLE_METERS, true)
             )
         );
     }
@@ -301,7 +300,7 @@ public class CommandFactory {
             new ConditionalCommand(new InstantCommand(), groundRetractedPosition(), elbowSubsystem::isSafeToExtendArm),
             new ParallelCommandGroup(
                 new PositionElbowCommand(elbowSubsystem, RobotConstants.ELBOW.POSITION_PRESETS.LOAD_STATION_DEGREES, true),
-                new PositionTelescopeCommand(armSubsystem, 1.4, true)
+                new PositionTelescopeCommand(armSubsystem, RobotConstants.ARM.POSITION_PRESETS.LOAD, true)
             )
         );
     }
