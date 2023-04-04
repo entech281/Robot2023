c122447d0b5444bf93cb67eb1748b71f3f546b97
Michael Andrews <mandrews@mandrews.org>
2023-04-03 22:14:03 -0400

    use the resetEncoders method

--- src/main/java/frc/robot/commands/DriveCSForwardDockCommand.java
+++ src/main/java/frc/robot/commands/DriveCSForwardDockCommand.java
@@ -11,16 +11,16 @@ import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.NavXSubSystem;
 
 /**
- * Assumes that the robot has already tipped the charging station.  
+ * Assumes that the robot has already tipped the charging station.
  * Moves about 1 robot depth on to the charging station
  */
 public class DriveCSForwardDockCommand extends EntechCommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final DriveSubsystem drive;
-  private final NavXSubSystem navx;
-  private double start_speed = 0.0;
-  private double end_speed = 0.0;
-  private double startDistance;
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final DriveSubsystem drive;
+    private final NavXSubSystem navx;
+    private double start_speed = 0.0;
+    private double end_speed = 0.0;
+    private double startDistance;
 
   /**
    * Creates a new DriveCSForwardDockCommand.
@@ -29,7 +29,7 @@ public class DriveCSForwardDockCommand extends EntechCommandBase {
    * @param nsubsys NavX subsystem used for pitch measurement
    */
   public DriveCSForwardDockCommand(DriveSubsystem dsubsys, NavXSubSystem nsubsys) {
-	  super(dsubsys);
+      super(dsubsys);
       this.drive = dsubsys;
       this.navx = nsubsys;
       double pitch = this.navx.getPitch();
@@ -45,47 +45,50 @@ public class DriveCSForwardDockCommand extends EntechCommandBase {
    * @param speed Drive speed (forward is positive, default)
    */
   public DriveCSForwardDockCommand(DriveSubsystem dsubsys, NavXSubSystem nsubsys, double start_speed, double end_speed) {
-    super(dsubsys);
-    this.drive = dsubsys;
-    this.navx = nsubsys;
-    double pitch = this.navx.getPitch();
-    this.start_speed = Math.copySign(start_speed, pitch);
-    this.end_speed = Math.copySign(end_speed, pitch);
+      super(dsubsys);
+      this.drive = dsubsys;
+      this.navx = nsubsys;
+      double pitch = this.navx.getPitch();
+      this.start_speed = Math.copySign(start_speed, pitch);
+      this.end_speed = Math.copySign(end_speed, pitch);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    drive.resetEncoders();
-    startDistance = drive.getAverageDistanceMeters();
-    startDistance = 0.0;
+      // If for some reason, resetting the encoders does not work properly, the command can track the distance itself by
+      // removing the first two lines and replacing them with the last line
+      drive.resetEncoders();
+      startDistance = 0.0;
+      // startDistance = drive.getAverageDistanceMeters();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double s = calculateSpeed(Math.abs(drive.getAverageDistanceMeters()-startDistance), RobotConstants.BALANCE_PARAMETERS.DOCK_DISTANCE, start_speed, end_speed);
-    DriveInput di=new DriveInput(s,0.0,0.0, navx.getYawAngleDegrees());
-    drive.driveFilterYawRobotRelative(di);
+      double s = calculateSpeed(Math.abs(drive.getAverageDistanceMeters()-startDistance), RobotConstants.BALANCE_PARAMETERS.DOCK_DISTANCE, start_speed, end_speed);
+      DriveInput di=new DriveInput(s,0.0,0.0, navx.getYawAngleDegrees());
+      drive.driveFilterYawRobotRelative(di);
   }
 
   private double calculateSpeed(double distance, double dref, double speed0, double speed1) {
-    double fraction = Math.min(distance/dref, 1.0);
-    return (1.0-fraction)*speed0 + (fraction*speed1);
+      double fraction = Math.min(distance/dref, 1.0);
+      return (1.0-fraction)*speed0 + (fraction*speed1);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) { 
+  public void end(boolean interrupted) {
+      // do not stop the robot.  We assume the next command will take over a moving robot and try the balance
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (Math.abs(drive.getAverageDistanceMeters()-startDistance) > RobotConstants.BALANCE_PARAMETERS.DOCK_DISTANCE) {
-        DriverStation.reportWarning("END:" + this, false);
-        return true;
-    }
+      if (Math.abs(drive.getAverageDistanceMeters()-startDistance) > RobotConstants.BALANCE_PARAMETERS.DOCK_DISTANCE) {
+          DriverStation.reportWarning("END:" + this, false);
+          return true;
+      }
       return false;
   }
 
